981af3593162cc6ec95f35686e1ec21cda0fa397
suvamcuelogic <85682643+suvamcuelogic@users.noreply.github.com>
2021-10-12 16:03:55 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,6 +1,9 @@
 def call(status,message){
- if(status=='success'){
-  echo "\033[34mHello\033[0m ${message}"
- }
+  if(status=='success'){
+      ansiColor('xterm')
+         {
+            echo "\033[34mHello\033[0m ${message}"
+         }
+  }
  
 }
